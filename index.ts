e2d11ec5b430f11f4b2cf3948538bd797edf322e
brandonm98 <brandon_m98@live.com.mx>
2024-05-12 12:00:22 -0600

    change back to express api

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import { APIGatewayProxyResultV2,APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import express from 'express';
+import serverless from 'serverless-http';
 
-export const handler: Handler = async (event: APIGatewayProxyEvent) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from lambda! CHANGE!!!!!'),
-    };
-    return response
-}
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Express on AWS Lambda!' });
+  });
+
+export const handler = serverless(app);
+
+module.exports.handler = serverless(app);
