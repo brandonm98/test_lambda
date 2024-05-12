1908a29f1f98f7e0928de4137ba2102506ed382a
brandonm98 <brandon_m98@live.com.mx>
2024-05-12 11:49:17 -0600

    run workflow

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyResultV2,APIGatewayProxyEvent, Handler } from 'aws-lambd
 export const handler: Handler = async (event: APIGatewayProxyEvent) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda! 123'),
+        body: JSON.stringify('Hello from lambda! CHANGE!!!!!'),
     };
     return response
 }
